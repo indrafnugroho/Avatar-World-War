9ac55f94a727d3394c7dbace81b5a8ddfedcbb4b
ManEnfu <hxrd97@gmail.com>
2019-10-18 19:02:26 +0700

    Add initial files

---
+++ headers/bool.h
@@ -0,0 +1,11 @@
+/* boolean.h */
+/* Definisi type boolean */
+
+#ifndef _BOOL_h
+#define _BOOL_h
+
+#define bool unsigned char
+#define true 1
+#define false 0
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,9 @@
+/* main.c */
+
+#include <stdio.h>
+#include "bool.h"
+
+int main() {
+    printf("%d %d", true, false);
+    return 0;
+}
