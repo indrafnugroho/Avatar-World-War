a3a2fff1cffc27d72f63024609aa1aa9b0514738
ManEnfu <hxrd97@gmail.com>
2019-10-22 13:13:48 +0700

    Add definition

--- headers/Matrix.h
+++ headers/Matrix.h
@@ -6,4 +6,35 @@ DEFINISI ADT MATRIX <Matrix>
 #ifndef _MATRIX_H
 #define _MATRIX_H
 
+typedef void* MatrixVal_t;
+typedef int MatrixIndex;
+typedef struct {
+    MatrixVal_t** M;
+    int MMax, NMax, MEff, NEff;
+} Matrix;
+
+#define MMax(m) m.MMax
+#define NMax(m) m.NMax
+#define MEff(m) m.MEff
+#define NEff(m) m.NEff
+#define MatrixElement(m, i, j) m.M[i][j]
+
+void MatrixCreate(Matrix* m, int mMax, int nMax);
+
+void MatrixSetStaticSize(Matrix* m, int mNew, int nNew);
+
+bool MatrixIsEmpty(Matrix m);
+
+bool MatrixIsFull(Matrix m);
+
+bool MatrixIsSquare(Matrix m);
+
+bool MatrixIsSimetrical(Matrix m);
+
+Matrix MatrixTranspose(Matrix m);
+
+void MatrixRealloc(Matrix* m, int mNew, int nNew);
+
+void MatrixDealloc(Matrix* m);
+
 #endif
--- headers/Point.h
+++ headers/Point.h
@@ -6,4 +6,14 @@ DEFINISI ADT POINT <Point>
 #ifndef _POINT_H
 #define _POINT_H
 
+typedef struct {
+    int x, y;
+} Point;
+
+#define PointX(p) p.x
+#define PointY(p) p.y
+
+void PointCreate(Point* P, int x, int y);
+void PointMove(Point* P, int dx, int dy);
+
 #endif
