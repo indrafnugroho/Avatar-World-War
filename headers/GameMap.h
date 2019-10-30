c0202368dce3f508989ab4a628763a26a6848dc8
Indra F Nugroho <indrafbrngrh@gmail.com>
2019-10-30 13:09:12 +0700

    added GameMap

---
+++ headers/GameMap.h
@@ -0,0 +1,17 @@
+/*
+GameMap.h
+DEFINISI ADT GAMEMAP <GameMap>
+*/
+
+#ifndef _GAMEMAP_H
+#define _GAMEMAP_H
+
+#include "bool.h"
+#include "pcolor.h"
+#include "Matrix.h"
+
+void PrintMap(Matrix m);
+// Mencetak peta permainan ke layar
+// dengan spesifikasi border * di sisi peta
+// dan Tipe Bangunan dengan tanda kepemilikan
+// warna pemain
