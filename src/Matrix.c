f1a04f2d73a6bc4128aa18d44fc06b4ea5f94fbd
Indra F Nugroho <indrafbrngrh@gmail.com>
2019-10-30 09:45:09 +0700

    Update Matrix.c

--- src/Matrix.c
+++ src/Matrix.c
@@ -21,7 +21,7 @@ void MatrixCreate(Matrix* m, int mMax, int nMax) {
 }
 
 void MatrixSetStaticSize(Matrix* m, int mNew, int nNew) {
-    
+
 }
 
 bool MatrixIsEmpty(Matrix m) {
@@ -37,16 +37,55 @@ bool MatrixIsSquare(Matrix m) {
 }
 
 bool MatrixIsSimetrical(Matrix m) {
-    
+    //Kamus Lokal
+    MatrixIndex i=1, j;
+    bool found=false;
+
+    //Algoritma
+    if (MatrixIsSquare(m)) {
+        while ((!found) && (i<=MEff(m))) {
+            j=i;
+            while ((!found) && (j<=NEff(m))) {
+                if (MatrixElement(m,i,j) != MatrixElement(m,j,i)) {
+                    found = true;
+                } else {
+                    j++;
+                }
+            } i++;
+        } 
+        return (!found);
+    }
+    else return false;
 }
 
-Matrix MatrixTranspose(Matrix m) {}
+Matrix MatrixTranspose(Matrix m) {
+    //Kamus Lokal
+    MatrixIndex i=1, j=1, k;
+    MatrixVal_t temp;
+    int tempInt;
+
+    //Algoritma
+    while (i<=MEff(m)) {
+        k=j;
+        while (k<=NEff(m)) {
+            temp=MatrixElement(m,i,k);
+            MatrixElement(m,i,k) = MatrixElement(m,k,i);
+            MatrixElement(m,k,i) = temp;
+            k++;
+        }
+        i++;
+        j++;
+    } 
+    tempInt = MEff(m);
+    MEff(m) = NEff(m);
+    NEff(m) = tempInt;
+}
 
 void MatrixRealloc(Matrix* m, int mNew, int nNew) {
     MMax(*m) = mNew;
     NMax(*m) = nNew;
 
-    M(*m) = realloc(M(*m), ((mMax+1) * sizeof(MatrixVal_t*)));
+    M(*m) = realloc(M(*m), ((mNew+1) * sizeof(MatrixVal_t*)));
 
     for (int i=1; i<=nNew; i++) {
         (*m).M[i] = realloc((*m).M[i], ((nNew+1)*sizeof(MatrixVal_t)));
