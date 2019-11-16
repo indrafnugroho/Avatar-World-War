2043b41a0f9045b39ba4b84811ad98efef29dde0
Asyifa <asyifa@Asyifas-MacBook-Pro.local>
2019-11-16 11:35:33 +0700

    Graph

--- src/Graph.c
+++ src/Graph.c
@@ -5,6 +5,7 @@ Body ADT GRAPH <Graph>
 
 #include "ArrayDin.h"
 #include "Graph.h"
+#include "List.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -40,20 +41,28 @@ void GraphAddEdge(Graph* g, GraphVertex v1, GraphVertex v2) {
     ListAddFirst(&Connect(*GraphVertexVal(*g, v2)), GraphVertexVal(*g, v1));
 }
 
-void GraphDelEdge(Graph* g, GraphVertex v1, GraphVertex v2){}
+void GraphDelEdge(Graph* g, GraphVertex v1, GraphVertex v2){
+    
+}
 
-bool GraphIsAdjacent(Graph g, GraphVertex v1, GraphVertex v2){}
+bool GraphIsAdjacent(Graph g, GraphVertex v1, GraphVertex v2){
+
+}
 
 void GraphDealloc(Graph* g){
     free(TI(*g));
 }
 
-void GraphAddVertex(Graph* g, GraphVertex* v, GraphVal_t x){}
+void GraphAddVertex(Graph* g, GraphVal_t x){
+  
+    AddAsLastEl(*g, x);
+}
 
-void GraphDelVertex(Graph* g, GraphVertex v, GraphVal_t* x){}
+void GraphDelVertex(Graph* g, GraphVal_t* x){
+    GraphVertexVal(*g, *v) = GraphVertexVal(*g, *v) - *x;
+    
+}
 
 GraphVertex GraphSearchVertex(Graph* g, GraphVal_t x){}
 
 void GraphRealloc(Graph* g, int MaxEl){}
-
-void GraphDealloc(Graph* g){}
