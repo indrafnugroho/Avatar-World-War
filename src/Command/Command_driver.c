6dbfb732802cd7d70528946b25e4a3d4346304a5
Rizky I. F <rykzius@gmail.com>
2019-11-23 15:07:54 +0700

    Command_driver lumayan selesai (?)

--- src/Command/Command_driver.c
+++ src/Command/Command_driver.c
@@ -3,9 +3,45 @@
 
 int test;
 int succeedCounter;
+Player P1;
+Player P2;
+ArrayDin Buildings;
+Building B1, B2, B3, B4, B5;
+GameState* State1;
+Graph Connection;
+
+void print_state() {
+    ListElement* El;
+    Building* B;
+    int i;
+    printf("Player 1:\n");
+    printf("    Buildings:\n");
+    i = 1;
+    ListTraversal (El, ListFirstElement(Buildings(P1)), El != Nil) {
+        B = ListElementVal(El);
+
+        printf("    %d. ", i);
+        PrintBuilding(*B);
+        i++;
+    }
+    if (!QueueIsEmpty(Skills(P1))) printf("    Queue Head: %d\n", ListElementVal(QueueHead(Skills(P1))));
+    printf("Player 2:\n");
+    printf("    Buildings:\n");
+    i = 1;
+    ListTraversal (El, ListFirstElement(Buildings(P2)), El != Nil) {
+        B = ListElementVal(El);
+
+        printf("    %d. ", i);
+        PrintBuilding(*B);
+        i++;
+    }
+    if (!QueueIsEmpty(Skills(P2))) printf("    Queue Head: %d\n", ListElementVal(QueueHead(Skills(P1))));
+    printf("\n");
+}
 
 void start_test(const char* name) {
     test++;
+    print_state();
     printf("Memulai tes %d: %s\n", test, name);
     printf("--------------------------------------------\n");
 }
@@ -14,6 +50,7 @@ void end_test() {
     printf("--------------------------------------------\n");
     printf("Tes %d berhasil diselesaikan.\n", test);
     printf("\n");
+    print_state();
     succeedCounter++;
 }
 
@@ -33,22 +70,33 @@ int main() {
     // SETUP KONDISI PENGUJIAN
 
     // --------------------------------------------------------------------------------
+    
+    // setup wajib awal
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
     // p1
-    Player P1;
     CreateNewPlayer(&P1);
 
     // p2
-    Player P2;
     CreateNewPlayer(&P2);
     
     // states
     Stack* States;
     StackCreate(States);
 
+    //
+    //   6         V
+    //   5       C
+    //   4     F
+    //   3   T
+    //   2 C
+    //   1   
+    //     1 2 3 4 5
+    //
+    // yang sebelahan ceritanya nyambung / bisa nyerang
+
     // buildings
-    ArrayDin Buildings;
-    Building B1, B2, B3, B4, B5;
     Type(B1) = 'C';
     PointCreate(&Koordinat(B1), 1, 2); 
     InitializationBuilding(&B1);
@@ -71,11 +119,31 @@ int main() {
     AddAsLastEl(&Buildings, &B4); // 3
     AddAsLastEl(&Buildings, &B5); // 4
 
+    // graph
+    GraphCreate(&Connection);
+    int i;
+    for (i = 0; i < 5; i++) {
+        GraphNewVertexLast(&Connection);
+    }
+    for (i = 0; i < 4; i++) {
+        GraphAddEdgeIdx(&Connection, i, i+1);
+        GraphAddEdgeIdx(&Connection, i+1, i);
+    }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
     // keadaan dulu
 
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
     // player 1 punya skill
-    AddSkill(&P1, 1);
     AddSkill(&P1, 2);
+
+    // b3 dan b5 level up
+    Troops(B3) = M(B3);
+    LevelAdd(&B3);
+    Troops(B5) = M(B5);
+    LevelAdd(&B5);
 
     // punya p1
     ListAddLast(&Buildings(P1), &B1);
@@ -86,14 +154,44 @@ int main() {
     ListAddLast(&Buildings(P2), &B4);
     ListAddLast(&Buildings(P2), &B5);
 
-    GameState* State1 = CreateGameState(P1, P2, Buildings);
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    // save state dulu
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    State1 = CreateGameState(P1, P2, Buildings);
     StackPush(States, State1);
 
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
     // keadaan sekarang
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     
-    // player 1 dipake skill pertamanya
+    // player 1 dipake 2 skill pertamanya
     int s;
     QueueDel(&Skills(P1), &s);
+    AddSkill(&P1, 3);
+
+    // b1, b2, dan b4 level up
+    Troops(B1) = M(B1);
+    LevelAdd(&B1);
+    Troops(B2) = M(B2);
+    LevelAdd(&B2);
+    Troops(B2) = M(B2);
+    LevelAdd(&B2);
+    Troops(B4) = M(B4);
+    LevelAdd(&B4);
+    Troops(B4) = M(B4);
+    LevelAdd(&B4);
+    Troops(B4) = M(B4);
+    LevelAdd(&B4);
+    printf("\n");
+
+    // b3 dan b5 siap level up
+    Troops(B3) = M(B3)/2;
+    Troops(B5) = M(B5)/2;
 
     // p1 menang 2 building, b3 dan b4
     ListAddLast(&Buildings(P1), &B3);
@@ -102,29 +200,6 @@ int main() {
     // p2 kalah 2 building, b3 dan b4
     ListDelVal(&Buildings(P2), &B3);
     ListDelVal(&Buildings(P2), &B4);
-
-    //
-    //   6         V
-    //   5       C
-    //   4     F
-    //   3   T
-    //   2 C
-    //   1   
-    //     1 2 3 4 5
-    //
-    // yang sebelahan ceritanya nyambung / bisa nyerang
-
-    // graph
-    Graph Connection;
-    GraphCreate(&Connection);
-    int i;
-    for (i = 0; i < 5; i++) {
-        GraphNewVertexLast(&Connection);
-    }
-    for (i = 0; i < 4; i++) {
-        GraphAddEdgeIdx(&Connection, i, i+1);
-    }
-
 
     // --------------------------------------------------------------------------------
 
@@ -141,167 +216,12 @@ int main() {
     // GState = States
     // Connect = Connection
 
+    printf("Command List: ATTACK LEVEL_UP SKILL UNDO END_TURN SAVE MOVE EXIT\n");
+    printf(" - Bangunan yang sebelahan bisa move / attack\n");
+    printf(" - Bangunan Fort dan Village siap level up\n");
     int a = InputCommand(&P1, &P2, &Buildings, States, Connection);
-    printf("Hasil: %d\n", a);
-
-    end_test();
-
-    // --------------------------------------------------------------------------------
-
-    // void AttackCommand(Player* PTurn, Player* PEnemy, ArrayDin Bldgs, Graph Connect);
-    /* Melakukan mekanisme attack apabila user menginput command Attack,
-    yaitu mencetak daftar bangunan yang dimiliki user,
-    opsi untuk menyerang dengan bangunan apa,
-    daftar bangunan yang dapat diserang (yang saling terhubung),
-    opsi bangunan apa yang akan diserang,
-    jumlah pasukan yang digunakan untuk menyerang,
-    dan hasil akhir penyerangan. */
-
-    // --------------------------------------------------------------------------------
-
-    start_test("Perintah attack di giliran player 1.");
-    // Isi parameter:
-    // PTurn = &P1
-    // PEnemy = &P2
-    // Bldgs = Buildings
-    // Connect = Connection
-
-
-
-    end_test();
-
-    // --------------------------------------------------------------------------------
-
-    // void LevelUpCommand(Player* PSelf);
-    /* Melakukan mekanisme Level_Up apabila user menginput command Level_UP,
-    yaitu mencetak daftar bangunan yang dimliki user,
-    opsi untuk level up bangunan yang mana,
-    dan hasil akhir level up. */
-
-    // --------------------------------------------------------------------------------
-
-    start_test("Perintah level up di player 1.");
-    // Isi parameter:
-    // PSelf = &P1
-
-
-
-    end_test();
-
-    // --------------------------------------------------------------------------------
-
-    // void SkillCommand(Player* PTurn, Player* PEnemy);
-    /* Melakukan mekanisme Skill apabila user menginput command Skill,
-    yang disesuaikan dengan efek Skill masing-masing */
-
-    // --------------------------------------------------------------------------------
-
-    start_test("Perintah gunakan skill di giliran player 1.");
-    // Isi parameter:
-    // PTurn = &P1
-    // PEnemy = &P2
-
-
-
-    end_test();
-
-    // --------------------------------------------------------------------------------
-
-    // void UndoCommand(Player* PTurn, Player* PEnemy, ArrayDin* Bldgs, Stack* GState);
-    /* Melakukan mekanisme Undo apabila user menginput command Undo.
-    User hanya dapat melakukan UNDO hingga command sesudah END_TURN / SKILL. 
-    Artinya, setelah command END_TURN / SKILL, pemain tidak dapat melakukan UNDO lagi */
-
-    // --------------------------------------------------------------------------------
-
-    start_test("Perintah undo di giliran player 1.");
-    // Isi parameter:
-    // PTurn = &P1
-    // PEnemy = &P2
-    // Bldgs = &Buildings
-    // GState = States
-
-
-
-    end_test();
-
-    // --------------------------------------------------------------------------------
-
-    // void EndTurnCommand(Player* PTurn, Player* PEnemy);
-    /* Melakukan mekanisme End_Turn apabila user menginput command End_Turn. */
-
-    // --------------------------------------------------------------------------------
-
-    start_test("Perintah end turn di giliran player 1.");
-    // Isi parameter:
-    // PTurn = &P1
-    // PEnemy = &P2
-
-
-
-    end_test();
-
-    // --------------------------------------------------------------------------------
-
-    // void MoveCommand(Player* PSelf, ArrayDin Bldgs, Graph Connect);
-    /* Melakukan mekanisme Move, yaitu memindahkan pasukan dari suatu bangunan ke bangunan lain milik
-    pemain yang terhubung dengan bangunan tersebut. MOVE hanya dapat dilakukan
-    sekali untuk tiap bangunan pada tiap gilirannya. */
-
-    // --------------------------------------------------------------------------------
-
-    start_test("Perintah move di giliran player 1");
-    // Isi parameter:
-    // PSelf = &P1
-    // Bldgs = &Buildings
-    // Connect = Connection
-
-
-
-    end_test();
-
-    // --------------------------------------------------------------------------------
-
-    // void ExitCommand();
-    /* Melakukan mekanisme Exit, yaitu keluar dari permainan */
-
-    // --------------------------------------------------------------------------------
-
-    start_test("Perintah exit di giliran player 1.");
-
-
-
-    end_test();
-
-    // --------------------------------------------------------------------------------
-
-    // void AddAToAllBuilding (Player* P);
-    /* Melakukan penambahan pasukan (A) terhadap bangunan player */
-
-    // --------------------------------------------------------------------------------
-
-    start_test("Perintah penambahan pasukan kepada player 1.");
-    // Isi parameter:
-    // P = &P
-
-
-
-    end_test();
-
-    // --------------------------------------------------------------------------------
-
-    // void SetToFalse (Player* P);
-    /*  I.S. Player terdefinisi.
-        F.S. Set All AfterAttack and AfterMove All Player's Building to False
-    */
-
-    // --------------------------------------------------------------------------------
-
-    start_test("SetToFalse pada player 1.");
-    // Isi parameter:
-    // P = &P1
-
-
+    printf("--------------------------------------------\n");
+    printf("Output perintah: %d\n", a);
 
     end_test();
 
