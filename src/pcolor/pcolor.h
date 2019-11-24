5a5c7aa42b8ecc65f0bd9d803f21767f0340c1c9
ManEnfu <hxrd97@gmail.com>
2019-11-24 12:02:38 +0700

    Add spec

--- src/pcolor/pcolor.h
+++ src/pcolor/pcolor.h
@@ -31,12 +31,34 @@ We only provide 8 colors. Feel free to modify/add colors if you want to.
 #define BG_WHITE "\x1B[47m"
 #define BOLD "\x1B[1m"
 #define UNDERLINE "\x1B[4m"
+
 void print_red(char c);
+/*Mencetak karakter warna merah */
 void print_green(char c);
+/*Mencetak karakter warna hijau */
 void print_yellow(char c);
+/*Mencetak karakter warna kuning */
 void print_blue(char c);
+/*Mencetak karakter warna biru */
 void print_magenta(char c);
+/*Mencetak karakter warna magenta */
 void print_cyan(char c);
+/*Mencetak karakter warna cyan */
 void print_color(char c, char* color);
+/*Mencetak karakter dengan warna yang terdefinisi */
 void set_print_color(char* color);
+/*
+I.S.
+    sembarang
+F.S.
+    pemanggilan printf selanjutnya akan mencetak string dengan warna yang
+    ditentukan dalam pemanggilan fungsi ini
+*/
 void reset_print_color();
+/*
+I.S.
+    telah dipanggil prosedur set_print_color() sebelumnya
+F.S.
+    mengembalikan kondisi sehingga pemanggilan printf akan mencetak warna default
+*/
+
